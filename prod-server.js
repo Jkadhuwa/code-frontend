d425e04815feeb5521136987fab7cefb74bb0c1c
Musinda Kadhuwa <kadhuwa.musinda@andela.com>
2019-11-14 00:22:41 +0200

    fix heroku build error

--- prod-server.js
+++ prod-server.js
@@ -4,10 +4,10 @@ import path from "path";
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/build"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.resolve(__dirname, "build/index.html"));
 });
 
 app.listen(port, () => {
